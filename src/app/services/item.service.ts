6838f9d67b785eda6d516b66ca13d0087926f07c
Paulo Tenório <paulorltenorio@gmail.com>
2025-02-09 20:44:07 -0300

    Editei o arquivo src/app/services/item.service.ts para incluir os métodos necessários para consumir as APIs.

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
+  private readonly apiUrl = 'http://127.0.0.1:8000/api/items'; // URL do backend Laravel
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+    // Método para listar todos os itens
+    getItems(): Observable<any> {
+      return this.http.get(this.apiUrl);
+    }
+    // Método para obter um item específico
+    getItemById(id: number): Observable<any> {
+      return this.http.get(`${this.apiUrl}/${id}`);
+    }
+    // Método para criar um novo item
+    createItem(data: any): Observable<any> {
+      return this.http.post(this.apiUrl, data);
+    }
+    // Método para atualizar um item
+    updateItem(id: number, data: any): Observable<any> {
+      return this.http.put(`${this.apiUrl}/${id}`, data);
+    }
+    // Método para deletar um item
+    deleteItem(id: number): Observable<any> {
+      return this.http.delete(`${this.apiUrl}/${id}`);
+    }
+
 }
